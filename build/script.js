cb5b470604b4db1a8303d809ff32a7b3a1e15d89
EveryOne <sixwaysband@gmail.com>
2020-01-19 01:57:51 +0300

    рефакторинг js и Фиксы по TODO - js отформатирован для лучшей читаемости - стили отсупов текста через обход мапа - цвет текста чере css-var

--- build/script.js
+++ build/script.js
@@ -1,9 +1,38 @@
+document.addEventListener('DOMContentLoaded', accordionDefault);
+document.body.addEventListener('click', bodyHandler);
+
+const handlerMap = {
+    'onoffswitch' : onoffswitchAction,
+    'e-accordion__short' : accordionAction,
+}
+
 const classMap = {
     'theme_color_project-default' : 'theme_color_project-inverse',
     'theme_color_project-inverse' : 'theme_color_project-default'
 }
 
-const onoffswitchAction = function (target) {
+function accordionDefault() {
+
+    let accordionsForDefault = document.querySelectorAll('.e-accordion__more');
+
+    accordionsForDefault.forEach( block => {
+        block.classList.add('e-accordion__more_state_close');
+    });
+
+}
+
+function bodyHandler(event) {
+
+    for( let className in handlerMap){
+        let target = event.target.closest('.'+className)
+        if (target) {
+            handlerMap[className](target);
+            break;
+        }
+    }
+}
+
+function onoffswitchAction(target) {
 
     target.classList.toggle('onoffswitch_checked');
 
@@ -23,17 +52,7 @@ const onoffswitchAction = function (target) {
 
 }
 
-const accordionDefault = function(){
-
-    let accordionsForDefault = document.querySelectorAll('.e-accordion__more');
-
-    accordionsForDefault.forEach( block => {
-        block.classList.add('e-accordion__more_state_close');
-    });
-
-}
-
-const accordionAction = function (target){
+function accordionAction (target){
 
     let forClose = document.querySelector('.e-accordion__more_state_open');
     if(forClose){
@@ -45,22 +64,3 @@ const accordionAction = function (target){
     forOpen.classList.remove('e-accordion__more_state_close');
     forOpen.classList.add('e-accordion__more_state_open');
 }
-
-const handlerMap = {
-    'onoffswitch' : onoffswitchAction,
-    'e-accordion__short' : accordionAction,
-}
-
-const bodyHandler = function (event) {
-
-    for( let className in handlerMap){
-        let target = event.target.closest('.'+className)
-        if (target) {
-            handlerMap[className](target);
-            break;
-        }
-    }
-}
-
-document.addEventListener('DOMContentLoaded', accordionDefault);
-document.body.addEventListener('click', bodyHandler);
