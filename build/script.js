8b5a0ee9f84668b5a036854ecaf45f689e995147
EveryOne <sixwaysband@gmail.com>
2020-01-18 20:12:28 +0300

    Реализованно переключение тем: - обработчик заменяет классы тем исходя из карты - на блоки header footer и layout добавлен дефолтный фон - фикс бордеров

--- build/script.js
+++ build/script.js
@@ -1,5 +1,28 @@
-const onoffswitchAction = function (target){
-    alert('test!');
+const themeBlocks = document.getElementsByClassName('theme');
+
+const classMap = {
+    'theme_color_project-default' : 'theme_color_project-inverse',
+    'theme_color_project-inverse' : 'theme_color_project-default'
+}
+
+const onoffswitchAction = function (target) {
+
+    target.classList.toggle('onoffswitch_checked');
+
+    let themeForInvert = Array.prototype.slice.call(themeBlocks);
+
+    themeForInvert.forEach( block => {
+
+        for( let className in classMap){
+            if (block.classList.contains(className)) {
+                block.classList.remove(className);
+                block.classList.add(classMap[className]);
+                break;
+            }
+        }
+
+    });
+
 }
 
 const accordionAction = function (target){
